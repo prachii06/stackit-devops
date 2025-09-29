a62129007ab04d7ec1e3e997ad9f37e460efc9a9
prachii06 <prachigohil201@gmail.com>
2025-09-29 16:53:00 +0530

    feat:add comments in network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,3 +1,4 @@
+#to create our isolated network
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   tags = {
@@ -5,15 +6,17 @@ resource "aws_vpc" "main" {
   }
 }
 
+#public subnet in vpc with 24 bit mask for servers,etc like internet-faciing things so that the user can make api calls to ec2
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
+  cidr_block              = "10.0.1.0/24"       
+  map_public_ip_on_launch = true             #to assign public ip to instances launched in this subnet
   tags = {
     Name = "StackIt-Public-Subnet"
   }
 }
 
+#connects our vpc to the internet
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
   tags   = {
@@ -21,6 +24,7 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
+#route table to route traffic from subnert to internet gateway
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
 
@@ -34,6 +38,7 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
+#associates route table with public subnet
 resource "aws_route_table_association" "public_assoc" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public_rt.id
