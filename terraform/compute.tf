6ac4b508b561bc76837c633f7b4e5ded8f38c24a
prachii06 <prachigohil201@gmail.com>
2025-10-02 16:31:48 +0530

    feat: associate key pair with EC2 instance

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -43,6 +43,7 @@ resource "aws_key_pair" "stackit_key" {
 resource "aws_instance" "stackit_server"{
     ami = "ami-0f918f7e67a3323f0"
     instance_type = "t3.micro"
+    key_name = aws_key_pair.stackit_key.key_name
 
     subnet_id              = aws_subnet.public.id
     vpc_security_group_ids = [aws_security_group.ec2_sg.id]
