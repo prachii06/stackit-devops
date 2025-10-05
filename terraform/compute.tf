85126aa856055525fb5a4dbfabca077b4467f017
prachii06 <prachigohil201@gmail.com>
2025-10-05 19:07:35 +0000

    feat: add comments

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "ec2_sg" {
         from_port = 22
         to_port = 22
         protocol = "tcp"
-        cidr_blocks = ["59.184.96.170/32"]
+        cidr_blocks = ["59.184.96.170/32"]   
 
     }
 
